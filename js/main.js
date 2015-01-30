05776bd31c2bc8f7b0c6fde818d6e26b8cac720b
Angel Alarcon <angelalarcon.aa@gmail.com>
2015-01-30 10:34:44 -0430

    menu fixed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
 $(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
+	var scroll = $(window).scrollTop();
 
-    if (scroll >= 500) {
-        $("header").addClass("show");
-    } else {
-        $("header").removeClass("show");
-    }
+	if (scroll >= 500) {
+		$("header").addClass("show");
+	} else {
+		$("header").removeClass("show");
+	}
+});
+
+
+$(window).on('scroll', function() {
+	if ($(window).scrollTop() > $('#wheretogo').height()) {
+		$('.black').addClass('fixed');
+	}
+	else {
+		$('.black').removeClass('fixed');
+	}
+});
+
+var offset = 39;
+
+$('.black a').click(function(event) {
+    event.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView();
+    scrollBy(0, -offset);
 });
